51f338c4cf65cf88437b75dbfd33677a708e4e43
harshaathnere <harshaathnere@gmail.com>
2023-08-03 22:42:53 +0530

    Header part

--- App.js
+++ App.js
@@ -4,19 +4,24 @@
 // {id: "parent"}, 
 // React.createElement('div',{id: "Child"}, ), 
 // React.createElement("h1",{},"I am h1 child" ))
+
+
+// Header
+//logo
+// Nav items
 import React from "react";
 //import  ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div',
-    { id: "parent" },
-    [React.createElement('div', { id: "Child1" }, [
+// const parent = React.createElement('div',
+//     { id: "parent" },
+//     [React.createElement('div', { id: "Child1" }, [
 
-        React.createElement("h1", {}, "I am h1 child"),
-        React.createElement("h1", {}, "I am h2 child")]),
-    React.createElement('div', { id: "Child2" }, [
+//         React.createElement("h1", {}, "I am h1 child"),
+//         React.createElement("h1", {}, "I am h2 child")]),
+//     React.createElement('div', { id: "Child2" }, [
 
-        React.createElement("h1", {}, "I am h1 child"),
-        React.createElement("h1", {}, "I am h2 child")])])
+//         React.createElement("h1", {}, "I am h1 child"),
+//         React.createElement("h1", {}, "I am h2 child")])])
 
 
 
@@ -24,8 +29,53 @@ const parent = React.createElement('div',
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-// JSX
-const jstheading =<h1 id="heading"> JSx syntax </h1>
-//root.render(heading);
-console.log(parent);
-root.render(parent);
+// // JSX
+// const jstheading =<h1 id="heading"> JSx syntax </h1>
+
+
+// const heading =(
+//     <h1> React Element</h1>
+// );
+// const Heading = ()=> (
+// <h1> Functional Component</h1>
+// )
+// //root.render(heading);
+// console.log(parent);
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://logo.com/image-cdn/images/kts928pd/production/7ab5def0ab1dad26a90bc35cb7eed9e628f7f201-430x430.png?w=1080&q=72"/>
+            </div>
+            <div className="nav-items">
+               <ul>
+                <li>Home </li>
+                <li>About Us</li>
+                <li>Contact us</li>
+                <li>Cart</li>
+               </ul>
+            </div>
+        </div>
+    )
+}
+
+
+const Applayout = () => {
+    return (
+        <div className="app">
+<Header/>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+root.render(<Applayout/>);
--- Episode3.md
+++ Episode3.md
@@ -1,2 +1,10 @@
 Ecmascript - ES6, Javacsript and Browser understand Ecmascript
-JSx (Transplied before it reaches the JS engine)
+JSx (Transplied before it reaches the JS engine) Parcel- Babel,
+Babel is javascript Package 
+// JSX ==> Babel transpiles it to React.createElement => ReactElement JsObject => HTMLElement(render)
+
+React Component
+1. Class Component, functional Component 
+
+A function which is return JSX is functional Componet.
+Component Composition
